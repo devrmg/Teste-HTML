60443a0b52bbbcd80f0fe758dc0862cf9a4c3af7
Renan Mendes <renanmendescontact@gmail.com>
2019-10-25 20:40:05 -0300

    up

---
+++ js/skills.js
@@ -0,0 +1,21 @@
+var lang = {
+  "html": "90%",
+  "css": "90%",
+  "javascript": "25%",
+  "php": "55%",
+  "angular": "65%"
+};
+
+var multiply = 4;
+
+$.each( lang, function( language, pourcent) {
+
+  var delay = 700;
+  
+  setTimeout(function() {
+    $('#'+language+'-pourcent').html(pourcent);
+  },delay*multiply);
+  
+  multiply++;
+
+});
